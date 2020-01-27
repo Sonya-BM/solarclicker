b0a0ac19d7020dca48d2fdc1e864cea5bbf89b07
Slimen <metatidj.slimen@gmail.com>
2020-01-27 16:16:27 +0100

    maj js lundi 27 aprem

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var speed = 0;
 var prixAutoClick = 20;
+var prixMulti = 100;
 var cpt = 0;
 var cptAutoClick = 0;
+var cptMultipli = 0;
+var increment = 1;
 
 function vitess() {
-    speed = speed + 1;
+    speed = speed + increment;
     document.getElementById('vitesse').innerHTML = speed + 'km/h';
     console.log(speed);
 }
@@ -16,11 +19,30 @@ function autoClick() {
         speed = speed - prixAutoClick;
         prixAutoClick = prixAutoClick * 2;
 
-        document.getElementById('nombre_actif').innerHTML = cptAutoClick;
-        document.getElementById('vitesse').innerHTML = speed;
-        document.getElementById('prix_auto_click').innerHTML = prixAutoClick + 'km/h';
-        document.getElementById('prix_auto_click_suivant').innerHTML = prixAutoClick * 2 + 'km/h';
-        setInterval(autoClick, 1000);
+        document.getElementById('nombre_actif').innerHTML = 'Actif: ' + cptAutoClick;
+        document.getElementById('vitesse').innerHTML = speed + 'km/h';
+        document.getElementById('prix_auto_click').innerHTML = 'Actuel: ' + prixAutoClick + 'km/h';
+        document.getElementById('prix_auto_click_suivant').innerHTML ='Prochain: ' + prixAutoClick * 2 + 'km/h';
+        setInterval(vitess, 1000);
         console.log(speed)
+    }
+}
+
+function multipli(){
+
+    if (speed >= prixMulti){
+        cptMultipli = cptMultipli + 1;
+        speed = speed - prixMulti;
+        prixMulti = prixMulti * 2;
+
+        increment = increment * 1.5;
+        console.log(increment);
+
+       
+        document.getElementById('vitesse').innerHTML = speed + 'km/h';
+
+        document.getElementById('nombre_multi_actif').innerHTML = 'Actif: ' + cptMultipli;
+
+        document.getElementById('prix_multi_suivant').innerHTML = 'Prochain : ' + prixMulti + 'km/h';
     }
 }
